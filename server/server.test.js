bbcc48bfa4fe4181448f38c1f97630b7ca28e532
emeery <gerardoemery@live.com.mx>
2018-11-15 13:57:41 -0600

    describe

--- server/server.test.js
+++ server/server.test.js
@@ -1,26 +1,34 @@
 const request = require('supertest');
 const expect = require('expect');
 var app = require('./server').app;
-it('deberia devolver hola mundo response', (done) => {
-    request(app)
-    .get('/')
-    .expect((res) => {
-        expect(404)
-        expect(res.body).toInclude({
-            error: 'Not Found'
+
+describe('Servidor', () => {
+    describe('GET /', () => {
+        it('deberia devolver hola mundo response', (done) => {
+            request(app)
+            .get('/')
+            .expect((res) => {
+                expect(404)
+                expect(res.body).toInclude({
+                    error: 'Not Found'
+                });
+            })
+            .end(done);
         });
-    })
-    .end(done);
-});
-it('deberia devolver arreglo', (done) => {
-    request(app)
-    .get('/usuarios')
-    .expect(200)
-    .expect((res) => {
-        expect(res.body).toInclude({
-            nombre: 'Gerardo',
-            edad: 28
+    });
+    describe('GET /usuarios', () => {
+        it('deberia devolver arreglo', (done) => {
+            request(app)
+            .get('/usuarios')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body).toInclude({
+                    nombre: 'Gerardo',
+                    edad: 28
+                });
+            })
+            .end(done);
         });
-    })
-    .end(done);
+    });
 });
+
